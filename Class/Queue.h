a8c7aab954b6acae25cf20086d24a804cbc26f66
123123 <31946770+123123-github@users.noreply.github.com>
2017-12-25 16:16:06 +0800

    class_modification
    SqStack & LinkQueue

--- Class/Queue.h
+++ Class/Queue.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <iostream>
+
 //ADT Queue
-//C++ 类
+//C++ 类 队列实现
 
 //------------------ 队列结点定义 ------------------
 
@@ -27,16 +29,17 @@ private:
 
 public:
 	Queue();
+	Queue(const Queue& Q);
 	~Queue();
 
 public:
 	bool Empty();
-	bool Pop(ElemType &e);
+	ElemType Pop();
 	bool Push(ElemType e);
 };
 
 template<typename ElemType>
-inline bool MakeQNode(QNode<ElemType>*& p, ElemType e)
+inline bool MakeQNode(QNode<ElemType>*& p, ElemType e)		//分配队列结点
 {
 	p = new QNode<ElemType>;
 	if (!p)
@@ -44,4 +47,99 @@ inline bool MakeQNode(QNode<ElemType>*& p, ElemType e)
 
 	p->data = e;
 	p->next = nullptr;
+
+	return true;
+}
+
+
+//-------------------- 队列基本操作实现 --------------------
+
+template<typename ElemType>
+Queue<ElemType>::Queue()							//构造
+{
+	front = new QNode<ElemType>;
+
+	front->next = nullptr;
+	rear = front;
+}
+
+template<typename ElemType>
+inline Queue<ElemType>::Queue(const Queue & Q)		//拷贝构造
+{
+	Queueptr p = Q.front->next, s;
+
+	front = new QNode<ElemType>;
+	front->next = NULL;
+	rear = front;
+	
+	while (p)
+	{
+		MakeQNode(s, p->data);			//复制数据
+		rear->next = s;
+		rear = s;
+
+		p = p->next;
+	}
+}
+
+template<typename ElemType>
+Queue<ElemType>::~Queue()							//析构
+{
+	Queueptr p, q;
+	p = q = front;
+
+	while (p)
+	{
+		q = p->next;
+		delete p;
+		p = q;
+	}
+
+	front = nullptr;
+	rear = nullptr;
+}
+
+
+//-------- 基本函数操作 --------
+
+template<typename ElemType>
+bool Queue<ElemType>::Empty()						//判断是否空
+{
+	if (front == rear)
+		return true;
+	else
+		return false;
+}
+
+template<typename ElemType>
+ElemType Queue<ElemType>::Pop()				//出队
+{
+	if (front == rear)		//队为空则不能出队
+		return NULL;
+
+	Queueptr p;
+	ElemType e;
+	p = front->next;
+	e = p->data;
+
+	if (p == rear)			//如果 rear 被删，则将其指向 front
+		rear = front;
+
+	front->next = p->next;
+	delete p;
+	p = nullptr;
+
+	return e;
+}
+
+template<typename ElemType>
+bool Queue<ElemType>::Push(ElemType e)				//入队
+{
+	Queueptr s;
+	MakeQNode(s, e);
+
+	rear->next = s;
+	rear = s;
+
+	return true;
 }
--- Class/Stack.h
+++ Class/Stack.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 //--------------- 栈定义 ---------------
 
@@ -17,6 +18,7 @@ private:
 
 public:
 	Stack(int size = STACK_INIT_SIZE);
+	Stack(const Stack& S);
 	~Stack();
 
 public:
@@ -25,3 +27,97 @@ public:
 	bool Push(ElemType e);
 	bool GetTop(ElemType &e);
 };
+
+
+//--------------- 栈基本操作实现 -----------------
+
+template<class ElemType>
+Stack<ElemType>::Stack(int size)						//构造
+{
+	base = new ElemType[size];
+	top = base;
+	stacksize = size;
+}
+
+template<class ElemType>
+inline Stack<ElemType>::Stack(const Stack & S)			//拷贝构造函数
+{
+	stacksize = S.stacksize;
+	base = new ElemType[stacksize];
+
+	top = base + (S.top - S.base);
+	
+	for (ElemType *p = base, *q = S.base; p != top; p++, q++)
+		*p = *q;
+}
+
+template<class ElemType>
+Stack<ElemType>::~Stack()								//析构
+{
+	delete base;
+
+	base = nullptr;
+	top = base;
+	stacksize = -1;
+}
+
+//-------- 基本函数操作 --------
+
+template<class ElemType>
+bool Stack<ElemType>::Empty()							//判断是否空
+{
+	if (base == top)
+		return true;
+	else
+		return false;
+}
+
+template<class ElemType>
+ElemType Stack<ElemType>::Pop()							//弹出
+{
+	ElemType e;
+
+	if (top == base)
+		return NULL;
+	else
+	{
+		top--;
+		e = *top;
+		return e;
+	}
+}
+
+template<class ElemType>
+bool Stack<ElemType>::Push(ElemType e)					//入栈
+{
+	if (top - base >= stacksize)		//若栈满
+	{
+		ElemType* newbase;										//申请新空间
+		newbase = new ElemType[stacksize + STACKINCREMENT];
+
+		for (int i = 0; i < stacksize; i++)						//复制元素
+			newbase[i] = base[i];
+
+		delete base;											//释放久空间
+
+		base = newbase;											//指向新空间
+		top = base + stacksize;
+
+		stacksize += STACKINCREMENT;							//栈长增加
+	}
+
+	*top = e;
+	top++;
+
+	return true;
+}
+
+template<class ElemType>
+bool Stack<ElemType>::GetTop(ElemType &e)				//取顶部元素
+{
+	if (top == base)
+		return false;
+
+	e = *(top - 1);
+	return true;
+}
